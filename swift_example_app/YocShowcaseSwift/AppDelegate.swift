fd0c7e2031d36d7c9378872b17028fa2a086a644
Marvin Zinowsky <marvin.zinowsky@yoc.com>
2022-04-12 17:02:50 +0200

    added hardcoded consent string

--- swift_example_app/YocShowcaseSwift/AppDelegate.swift
+++ swift_example_app/YocShowcaseSwift/AppDelegate.swift
@@ -14,6 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let visxSdkManager = VisxSDKManager.sharedInstance()
         visxSdkManager.initializeSDK()
+        UserDefaults.standard.set("CO7_Ji2O7_Ji2NSAAAENAwCAAAAAAAAAAAAAF5wCAAWgAyACIAIEATQA8gCdgLzAAAAA.IGLtV_T9fb2vj-_Z99_tkeYwf95y3p-wzhheMs-8NyZeH_B4Wv2MyvBX4JiQKGRgksjLBAQdtHGlcTQgBwIlViTLMYk2MjzNKJrJEilsbO2dYGD9Pn8HT3ZCY70-vv__7v3ff_3g",
+                              forKey: "IABTCF_TCString")
+        UserDefaults.standard.set(1, forKey: "IABTCF_gdprApplies")
         return true
     }
 
