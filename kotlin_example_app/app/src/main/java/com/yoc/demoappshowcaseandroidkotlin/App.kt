c85777a5b062d49377b7742434b1b87557d42d2f
Marvin Zinowsky <marvin.zinowsky@yoc.com>
2022-04-12 17:18:47 +0200

    added hardcoded consent string

--- kotlin_example_app/app/src/main/java/com/yoc/demoappshowcaseandroidkotlin/App.kt
+++ kotlin_example_app/app/src/main/java/com/yoc/demoappshowcaseandroidkotlin/App.kt
@@ -1,6 +1,7 @@
 package com.yoc.demoappshowcaseandroidkotlin
 
 import android.app.Application
+import androidx.preference.PreferenceManager
 import com.yoc.visx.sdk.VisxSDK
 import timber.log.Timber
 
@@ -15,6 +16,12 @@ class App : Application() {
         instance = this
         initTimber()
         VisxSDK.initialize(applicationContext, "yoc_site_id")
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(this) ?: return
+        with (sharedPref.edit()) {
+            putString("IABTCF_TCString", "CO7_Ji2O7_Ji2NSAAAENAwCAAAAAAAAAAAAAF5wCAAWgAyACIAIEATQA8gCdgLzAAAAA.IGLtV_T9fb2vj-_Z99_tkeYwf95y3p-wzhheMs-8NyZeH_B4Wv2MyvBX4JiQKGRgksjLBAQdtHGlcTQgBwIlViTLMYk2MjzNKJrJEilsbO2dYGD9Pn8HT3ZCY70-vv__7v3ff_3g");
+            putInt("IABTCF_gdprApplies", 1);
+            apply()
+        }
     }
 
     private fun initTimber() {
